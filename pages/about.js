176645ef449c9360da59973280dc4b933a0398fe
Pierre Lambert <pierre.lam91@gmail.com>
2019-06-19 23:49:31 +0200

    Used getInitialProps to fetch datas from github api

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,59 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
-const About = () => (
-  <Layout title="About">
-    <Link href="/">
-      <a>Go to home</a>
-    </Link>
-    <p>Javascript Programmer</p>
-    <img src="static/js-logo.png" alt="javascript logo" height="100" />
-  </Layout>
-);
-export default About;
+import { Component } from 'react';
+import fetch from 'isomorphic-unfetch';
+
+export default class about extends Component {
+  state = {
+    user: null
+  };
+
+  static async getInitialProps() {
+    const res = await fetch('https://api.github.com/users/shanbic');
+    const data = await res.json();
+    console.log(data);
+    return { user: data };
+  }
+  render() {
+    const { user } = this.props;
+    return (
+      <Layout title="About">
+        <div className="card">
+          <p>
+            <img src={user.avatar_url} alt="my avatar" />
+            <a href={user.html_url} target="_blank">
+              {user.login} from {user.location}
+            </a>
+          </p>
+        </div>
+        <style jsx>{`
+          img {
+            border-radius: 5px;
+            width: 20px;
+            margin-right: 10px;
+          }
+
+          .card p{
+            display: flex;
+            justify-content: center;
+            align-items:flex-end; 
+          }
+
+          .card a{
+            text-decoration: none;
+            color:inherit;
+            text-decoration: underline;
+          }
+
+          .card a:hover{
+            
+            color: blue
+
+          }
+
+
+        `}</style>
+      </Layout>
+    );
+  }
+}
