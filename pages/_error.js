29ce9b4119f16057ad768cde15893c9ea3e86533
Pierre Lambert <pierre.lam91@gmail.com>
2019-06-20 00:29:56 +0200

    Added blog and posts pages

--- pages/_error.js
+++ pages/_error.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout';
 export default ({ statusCode }) => (
   <Layout title="error">
     {statusCode
-      ? `Could not load you user data: Status Code ${statusCode}`
+      ? `Could not load your user data: Status Code ${statusCode}`
       : "Couldn't get that page, Sorry"}
   </Layout>
 );
---
+++ pages/blog.js
@@ -0,0 +1,22 @@
+import Layout from '../components/Layout';
+import Link from 'next/link';
+
+const PostLink = ({ title, slug }) => {
+  return (
+    <li>
+      <Link href={`/post?title=${title}`} as={slug}>
+        <a>{title} post</a>
+      </Link>
+    </li>
+  );
+};
+
+export default () => (
+  <Layout title="My Blog">
+    <ul>
+      <PostLink slug="react-post" title="React Post" />
+      <PostLink slug="angular-post" title="Angular Post" />
+      <PostLink slug="vue-post" title="Vue Post" />
+    </ul>
+  </Layout>
+);
